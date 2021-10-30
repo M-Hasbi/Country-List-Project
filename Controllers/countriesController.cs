1dcf9cb0df58dffe85a22bb4e488e04b9443bebb
M-Hasbi <kaynakhasbi1@gmail.com>
2021-10-30 22:49:12 +0200

    adjustment: WebConfig.cs change from {id} to {code} create: DestinationCountry.cs andits implementation in the CountriesController adjustment: Route path for List<Country> Get() method adjustment: Route path for DestinationCountry Get(string code) method

--- Controllers/countriesController.cs
+++ Controllers/countriesController.cs
@@ -11,36 +11,38 @@ namespace Country_List_Project.Controllers
     public class CountriesController : ApiController
     {
         List<Country> countries = new List<Country>();
+        List<DestinationCountry> destinationCountries = new List<DestinationCountry>();
         public CountriesController()
         {
             countries.Add(new Country { countryCodes = { "CAN", "USA", "MEX", "BLZ", "GTM", "SLV", "HND", "NIC", "CRI", "PAN" } });
+            destinationCountries.Add(new DestinationCountry {destination = "CAN", list = {"USA", "CAN"} });
+            destinationCountries.Add(new DestinationCountry { destination = "MEX", list = { "USA", "MEX" } });
+            destinationCountries.Add(new DestinationCountry { destination = "BLZ", list = { "USA", "MEX", "BLZ" } });
+            destinationCountries.Add(new DestinationCountry { destination = "GTM", list = { "USA", "MEX", "GTM" } });
+            destinationCountries.Add(new DestinationCountry { destination = "SLV", list = { "USA", "MEX", "GTM", "SLV" } });
+            destinationCountries.Add(new DestinationCountry { destination = "HND", list = { "USA", "MEX", "GTM", "HND" } });
+            destinationCountries.Add(new DestinationCountry { destination = "NIC", list = { "USA", "MEX", "GTM", "HND", "NIC"} });
+            destinationCountries.Add(new DestinationCountry { destination = "CRI", list = { "USA", "MEX", "GTM", "HND", "NIC", "CRI" } });
+            destinationCountries.Add(new DestinationCountry { destination = "PAN", list = { "USA", "MEX", "GTM", "HND", "NIC", "CRI", "PAN"  } });
+            
         }
 
-        // GET: api/Countries
+        // GET: countries
+        [Route("countries")]
+        [HttpGet]
         public List<Country> Get()
         {
             return countries;
         }
 
-        // GET: api/Countries/5
-        public string Get(int id)
+        // GET: code
+        [Route("{code}")]
+        [HttpGet]
+        public DestinationCountry Get(string code)
         {
-            return "value";
+            return destinationCountries.Where(x => x.destination==code).FirstOrDefault();
+            
         }
 
-        // POST: api/Countries
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/Countries/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Countries/5
-        public void Delete(int id)
-        {
-        }
     }
 }
--- Models/Country.cs
+++ Models/Country.cs
@@ -8,7 +8,8 @@ namespace Country_List_Project.Models
     public class Country
     {
         public List<string> countryCodes { get; set; } = new List<string>(10);
+        
 
-       
+
     }
 }
