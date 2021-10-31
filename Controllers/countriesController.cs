2290eebeccee5f300f3800857a957ba1ad7fbf36
M-Hasbi <kaynakhasbi1@gmail.com>
2021-10-31 06:53:49 +0100

    create: API help documentation

--- Controllers/countriesController.cs
+++ Controllers/countriesController.cs
@@ -7,12 +7,17 @@ using System.Net.Http;
 using System.Web.Http;
 
 namespace Country_List_Project.Controllers
-{
+{/// <summary>
+/// This is where you can find all the travel path information which our trucks are able to travelthrough. 
+/// </summary>
     public class CountriesController : ApiController
     {
         List<Country> countries = new List<Country>();
         List<DestinationCountry> destinationCountries = new List<DestinationCountry>();
-        public CountriesController()
+        /// <summary>
+        /// This is the database which created in the ctor(construction) of the CountriesController  
+        /// </summary>
+        public CountriesController() 
         {
             countries.Add(new Country { countryCodes = { "CAN", "USA", "MEX", "BLZ", "GTM", "SLV", "HND", "NIC", "CRI", "PAN" } });
             destinationCountries.Add(new DestinationCountry {destination = "CAN", list = {"USA", "CAN"} });
@@ -28,6 +33,10 @@ namespace Country_List_Project.Controllers
         }
 
         // GET: countries
+        /// <summary>
+        /// Gets a list of the countries which our trucks are able to travel through 
+        /// </summary>
+        /// <returns>A list of countries</returns>
         [Route("countries")]
         [HttpGet]
         public List<Country> Get()
@@ -35,6 +44,11 @@ namespace Country_List_Project.Controllers
             return countries;
         }
 
+        /// <summary>
+        /// Gets name of the destanation country and its border countries that our trucks are going to be passed through
+        /// </summary>
+        /// <param name="code">3 letters of the country code </param>
+        /// <returns> name of the destanation country and its border countries that our trucks are going to be passed through</returns>
         // GET: code
         [Route("{code}")]
         [HttpGet]
--- Models/Country.cs
+++ Models/Country.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Web;
 
 namespace Country_List_Project.Models
-{
+{/// <summary>
+/// The list of 3 letters codes of the countries that our trucks are able to travel through
+/// </summary>
     public class Country
-    {
+    {/// <summary>
+    /// 3 letters of the country codes
+    /// </summary>
         public List<string> countryCodes { get; set; } = new List<string>(10);
         
 
